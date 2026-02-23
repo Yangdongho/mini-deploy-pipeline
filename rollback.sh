8fb92c957e34779ff9dd96934778e602ae95f852
Yangdongho <ehdghqkqk@naver.com>
2026-02-23 21:22:36 +0900

    rollback: restore index.html from .bak

--- rollback.sh
+++ rollback.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+source ./config.env
+
+REMOTE_FILE="${DEST_PATH%/}/$(basename "$SRC_FILE")"
+BACKUP_FILE="${REMOTE_FILE}.bak"
 
 echo "call rollback"
 
-#add rollback logic
+ssh -o BatchMode=yes -o StrictHostKeyChecking=accept-new "root@${TARGET_HOST}" "test -f '${BACKUP_FILE}' && cp -f '${BACKUP_FILE}' '${REMOTE_FILE}' && systemctl restart nginx"
 
 exit 0
