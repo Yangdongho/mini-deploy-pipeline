a1eaaa8090e51b0000f3bfe78ed416cebe84a31a
Yangdongho <ehdghqkqk@naver.com>
2026-02-23 21:21:49 +0900

    deploy: add backup step

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@ set -u
 source ./config.env
 REMOTE_FILE="${DEST_PATH%/}/$(basename "$SRC_FILE")"
 
+#배포전 백업
+ssh -o BatchMode=yes -o StrictHostKeyChecking=accept-new \
+  "root@${TARGET_HOST}" \
+  "test -f '${REMOTE_FILE}' && cp -f '${REMOTE_FILE}' '${REMOTE_FILE}.bak' || true"
+
 scp -o BatchMode=yes -o StrictHostKeyChecking=accept-new \
   "$SRC_FILE" "root@${TARGET_HOST}:${REMOTE_FILE}" || exit 1
 
